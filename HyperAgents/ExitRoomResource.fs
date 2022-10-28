f30ff16413cc913176765436f009512da3f5ba42
Einar W. Høst <einarwh@gmail.com>
2022-10-28 21:15:38 +0200

    plane

---
+++ HyperAgents/ExitRoomResource.fs
@@ -0,0 +1,72 @@
+module ExitRoomResource
+
+open Chiron
+open Siren
+open TrappableRoomResource
+open Utils
+open Giraffe
+open HttpMethods
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Primitives 
+
+type RequestInfo = HttpContext * AgentColor * Agent<AgentResource.Message>
+type Message = RequestInfo * AsyncReplyChannel<HttpHandler>
+
+let roomInfo : RoomResourceUtils.RoomInfo = 
+  { name = "exit-room"
+    properties = 
+      { title = "The exit room." 
+        description = "You're in the exit room. Do you have the secret files? Then you should get out of here!" }
+    linkInfos = 
+      [ ("control-room", ["entrance"; "move"]) ]
+  }
+
+let agentRef = Agent<Message>.Start (fun inbox ->
+
+  let addPlaneLink (doc : SirenDocument) =
+    let planeLink = { rel = [ "move" ]; href = linkTo "plane"}
+    let links' = doc.links @ [planeLink]
+    { doc with links = links' }
+
+  let trappableRoomAgent = createAgent roomInfo
+  let rec loop() = async {
+    let! ((ctx, clr, agent), replyChannel) = inbox.Receive()
+    let! res = trappableRoomAgent.PostAndAsyncReply(fun ch -> ((ctx, clr, agent), ch))
+    (* Ask secret file where it is. *)
+    let! fileAt = SecretFileResource.agentRef.PostAndAsyncReply(fun ch -> SecretFileResource.LocationQuery ch)
+    let planeAvailable = 
+      match fileAt with
+      | SecretFileResource.SecretFileLocation.TakenByAgent agentColor 
+        when agentColor = clr ->
+        true
+      | SecretFileResource.SecretFileLocation.TakenByAgent agentColor ->
+        printfn "D'oh the %s agent has the secret file." agentColor
+        false
+      | SecretFileResource.SecretFileLocation.RoomLocation roomLoc ->
+        printfn "No one has the secret file - no one can leave."
+        false
+    let webPart = 
+      match res with 
+      | Ok doc ->
+        let doc' = if planeAvailable then addPlaneLink doc else doc
+        let s = doc' |> Json.serialize |> Json.format
+        Successful.OK s 
+      | Created (doc, loc) ->
+        let s = doc |> Json.serialize |> Json.format
+        Successful.CREATED s >=> setHttpHeader "location" (loc |> uri2str)
+      | Found loc ->
+        redirectTo false (loc |> uri2str)
+      | BadRequest why ->
+        RequestErrors.BAD_REQUEST why
+      | MethodNotAllowed why ->
+        RequestErrors.METHOD_NOT_ALLOWED why 
+      | InternalError why ->
+        ServerErrors.INTERNAL_ERROR why
+
+    webPart |> replyChannel.Reply 
+
+    return! loop()        
+    }
+
+  loop()
+)
---
+++ HyperAgents/PlaneResource.fs
@@ -0,0 +1,28 @@
+module PlaneResource
+
+open Chiron
+open Siren
+open Utils
+open RoomResourceUtils
+open Giraffe
+open Microsoft.AspNetCore.Http
+
+type Message = HttpContext * AsyncReplyChannel<HttpHandler>
+
+let doc : SirenDocument = 
+  { properties = 
+      { title = "The getaway plane!" 
+        description = "LOL you won IDK. The other agents will blow up or something." }
+    actions = []
+    links = [ selfLinkTo "plane" 
+              sirenLinkTo [ "restart" ] "start" ] }
+
+let agentRef = 
+  Agent<Message>.Start (fun inbox ->
+    let rec loop() = async {
+       let! (ctx, replyChannel) = inbox.Receive()
+       doc |> Json.serialize |> Json.format |> Successful.OK |> replyChannel.Reply
+       return! loop()
+    }
+    loop ())
+
